a09ba69d91a1ee1d24d02a0f6ff5472b485a8224
Mikhail Shustov <restrry@gmail.com>
2025-03-12 15:47:14 +0100

    add client_name with mcp_clickhouse (#21)

--- mcp_timeplus/mcp_env.py
+++ mcp_timeplus/mcp_env.py
@@ -121,6 +121,7 @@ class TimeplusConfig:
             "verify": self.verify,
             "connect_timeout": self.connect_timeout,
             "send_receive_timeout": self.send_receive_timeout,
+            "client_name": "mcp_clickhouse",
         }
 
         # Add optional database if set
